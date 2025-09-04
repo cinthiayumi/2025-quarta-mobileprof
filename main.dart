1ca58d59adc17b0b5b9acb5d47e41caf59292f53
Cinthia Yumi <132957194+cinthiayumi@users.noreply.github.com>
2025-09-04 00:03:27 +0000

    feat: if statement

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+
+     List<String?> alunos =[];
+     bool cadastrarOutroAluno = true;
+
+     while(cadastrarOutroAluno){
+     print("Informe o nome do aluno:");
+     String? nome = stdin.readLineSync();
+
+     alunos.add(nome);
+
+     print('Deseja cadastrar outro aluno?(S / N)');
+     var respostaUsuario + stdin.readLineSync();
+     cadastrarOutroAluno = respostaUsurio == "S";
+     
+     };
+}
---
+++ main2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main(){
+   // print( 'fim do programa');
+
+    int idade = 15;
+    if(idade <= 18)
+    {
+        print('fim do programa!');
+        return;
+
+}
+       print('pode entrar na balada');
+}
